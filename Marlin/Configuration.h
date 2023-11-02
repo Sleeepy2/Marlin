5f33f3a9b907da2e294d488a6862d85c4c452aed
Sleeepy2 <davids@mayerglobal.com>
2023-11-02 13:46:35 -0400

    Increased Min Extrude Temp and increased buffer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -868,7 +868,7 @@
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
 #define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 170
+#define EXTRUDE_MINTEMP 195
 
 /**
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
